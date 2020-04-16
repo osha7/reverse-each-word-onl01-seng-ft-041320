b45c68a7754f97f91add50fe89c394498c5c7d8f
Osha G <ogroetz@gmail.com>
2020-04-16 13:25:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(sentence)
-array << sentence.split (" ")
-array.each { |word| word.reverse}
+array << sentence.split(" ")
+array.each { |word| word.reverse }
+end
 
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  array.collect { |word| word.reverse }.join(" ")
 end
