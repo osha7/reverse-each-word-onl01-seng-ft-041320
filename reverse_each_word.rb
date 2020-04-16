3daa5666d40dc3faf26aec587605ec2a85a5da5a
Osha G <ogroetz@gmail.com>
2020-04-16 13:20:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
-def reverse_each_word(sentence)
+def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.collect do |string|
     new_array << string.reverse
   end
-    new_array.join " "
 
 end
